a5eb5b36ece03e3313b45c3f003db5c60e638384
yotalha <talha.coder9@gmail.com>
2021-01-06 23:07:02 +0500

    Chenged int data to obj
    made an Item class that replaces int data
    obj with id name and price.

--- CustomeDB/CustomeDB.cpp
+++ CustomeDB/CustomeDB.cpp
@@ -4,19 +4,28 @@
 #include <iostream>
 using namespace std;
 
+class Item {
+public:
+    string name;
+    int price;
+    int id;
+};
+
 class Node {
 public:
-    int data;
+    Item data;
     Node* next;
 };
 
 class LinkedList {
     Node* head = NULL;
 public:
-    void addAtEnd(int val) {
+    void addAtEnd(int id, string name, int price) {
 
         Node* new_node = new Node();
-        new_node->data = val;
+        new_node->data.id = id;
+        new_node->data.name = name;
+        new_node->data.price = price;
         new_node->next = NULL;
         if (head == NULL) {
             head = new_node;
@@ -39,17 +48,17 @@ public:
         }
     }
 
-    void deleteByValue(int x) {
+    void deleteById(int id) {
         if (head == NULL) {
             cout << "linked list is empty \n";
         }
-        else if (head->data == x) {
+        else if (head->data.id == id) {
             deleteAtHead();
         }
         else {
             Node* cur = head;
             bool found = true;
-            while (cur->data != x) {
+            while (cur->data.id != id) {
                 if (cur == NULL) {
                     found = false;
                     break;
@@ -65,7 +74,7 @@ public:
                 delete cur;
             }
             else {
-                cout << "val doesnt exist in linked list \n";
+                cout << "Id doesnt exist in linked list \n";
             }
         }
     }
@@ -73,13 +82,15 @@ public:
     void displayList() {
         Node* cur = head;
         while (cur != NULL) {
-            cout << cur->data << " ";
+            cout << cur->data.id << " ";
+            cout << cur->data.name << " ";
+            cout << cur->data.price << " \n";
             cur = cur->next;
         }
         cout << endl;
     }
 
-    void updateValue(int old_val, int new_val) {
+    void updatePrice(int old_val, int new_val) {
         Node* cur = head;
         bool found = false;
         if (head == NULL) {
@@ -87,15 +98,15 @@ public:
         }
         else {
             while (cur != NULL) {
-                if (cur->data == old_val) {
-                    cur->data = new_val;
+                if (cur->data.price == old_val) {
+                    cur->data.price = new_val;
                     found = true;
                     break;
                 }
                 cur = cur->next;
             }
             if (!found) {
-                cout << "value not found in linked iist.\n";
+                cout << "Price not found in linked iist.\n";
             }
         }
     }
@@ -105,12 +116,13 @@ public:
 int main()
 {
     LinkedList l1;
-    l1.addAtEnd(5);
-    l1.addAtEnd(70);
-    l1.deleteByValue(5);
+    l1.addAtEnd(1, "talha", 20);
+    l1.addAtEnd(2, "haseed", 10);
+    l1.addAtEnd(3, "bats", 25);
     l1.displayList();
-
-    l1.updateValue(70, 20);
+    l1.deleteById(1);
+    l1.displayList();
+    l1.updatePrice(10, 40);
     l1.displayList();
 
     

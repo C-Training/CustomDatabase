b852dabaec1acf2134dc6e81cd0d5ba81afc15f8
Talha Asjad <talha.asjad.ta@gmail.com>
2021-01-10 19:21:07 +0500

    Talha's linkedlist

--- CustomeDB/CustomeDB.cpp
+++ CustomeDB/CustomeDB.cpp
@@ -1,120 +1,158 @@
-// CustomeDB.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-class Node {
+class node {
 public:
-    int data;
-    Node* next;
-};
+	int data;
+	node* next;
 
-class LinkedList {
-    Node* head = NULL;
-public:
-    void addAtEnd(int val) {
+	void insertatstart(int x) {
 
-        Node* new_node = new Node();
-        new_node->data = val;
-        new_node->next = NULL;
-        if (head == NULL) {
-            head = new_node;
-        }
-        else {
-            Node* cur = head;
-            while (cur->next != NULL) {
-                cur = cur->next;
-            }
-            cur->next = new_node;
-        }
+		node* temp = new node;
+		temp->data = x;
+		temp->next = head;
+		head = temp;
+	}
 
-    }
-    void deleteAtHead() {
-        if (head == NULL) {
-            cout << "There is no linked list to delete" << endl;
-        }
-        else {
-            head = head->next;
-        }
-    }
+	void insertatend(int x) {
+		node* temp = new node;
+		temp->data = x;
+		temp->next = NULL;
+		if (head == NULL) {
+			head = temp;
+		}
+		else {
+			tail = head;
+			while (tail->next != NULL) {
+				tail = tail->next;
+			}
+			tail->next = temp;
+			tail = temp;
 
-    void deleteByValue(int x) {
-        if (head == NULL) {
-            cout << "linked list is empty \n";
-        }
-        else if (head->data == x) {
-            deleteAtHead();
-        }
-        else {
-            Node* cur = head;
-            bool found = true;
-            while (cur->data != x) {
-                if (cur == NULL) {
-                    found = false;
-                    break;
-                }
-                cur = cur->next;
-            }
-            if (found) {
-                Node* temp = head;
-                while (temp->next != cur) {
-                    temp = temp->next;
-                }
-                temp->next = cur->next;
-                delete cur;
-            }
-            else {
-                cout << "val doesnt exist in linked list \n";
-            }
-        }
-    }
+		}
 
-    void displayList() {
-        Node* cur = head;
-        while (cur != NULL) {
-            cout << cur->data << " ";
-            cur = cur->next;
-        }
-        cout << endl;
-    }
+	}
 
-    void updateValue(int old_val, int new_val) {
-        Node* cur = head;
-        bool found = false;
-        if (head == NULL) {
-            cout << "Linled list is empty" << endl;
-        }
-        else {
-            while (cur != NULL) {
-                if (cur->data == old_val) {
-                    cur->data = new_val;
-                    found = true;
-                    break;
-                }
-                cur = cur->next;
-            }
-            if (!found) {
-                cout << "value not found in linked iist.\n";
-            }
-        }
-    }
+	void deletehead() {
+		if (head == NULL) {
+			cout << "Link list is already ded. \n";
+		}
+		else {
+			//		node * temp = new node;
+			//		temp->next=head;
+			head = head->next;
+		}
+	}
+
+	void deleteend() {
+		node* temp = new node;
+		if (head == NULL) {
+			cout << "Link list is already ded";
+		}
+		else if (head->next == NULL) {
+			head = NULL;
+		}
+		else {
+			temp = head;
+			while (temp->next != NULL) {
+				tail = temp;
+				temp = temp->next;
+			}
+			tail->next = NULL;
+			delete temp;
+		}
+	}
+
+	void insertatnnode(int data, int pos) {
+		node* temp = new node;
+
+
+	}
+
+
+
+	void print() {
+		node* temp = head;
+		while (temp != NULL) {
+			cout << temp->data << " ";
+			temp = temp->next;
+		}
+	}
+	node* head = NULL;
+	node* tail = NULL;
 
 };
 
-int main()
-{
-    LinkedList l1;
-    l1.addAtEnd(5);
-    l1.addAtEnd(70);
-    l1.deleteByValue(5);
-    l1.displayList();
-
-    l1.updateValue(70, 20);
-    l1.displayList();
-
-    
 
 
-    return 0;
+
+
+
+
+
+//
+//deletestart(){
+//	if (head!=NULL){
+//	node*temp=head;
+//	head->next=head;
+//	delete head;	
+//	}
+//	
+//	
+//}
+
+
+
+
+
+
+
+
+int main() {
+
+	node list;
+	int size, num;
+
+	list.insertatstart(1);
+	list.insertatstart(2);
+	list.insertatstart(4);
+	list.insertatstart(5);
+	list.insertatstart(0);
+	list.deletehead();
+	list.deletehead();
+	list.print();
+
+	// list.insertatend(3);
+	// list.insertatend(2);
+	// list.insertatend(1);
+	// list.deleteend();
+	// list.deleteend();
+	// list.print();
+	//	
+	//	cout<<"Enter the size of your linked list : ";
+	//	cin>>size;
+	//	cout<<"Enter numbers at start"<<endl;
+	//	for(int i=0; i<size; i++){
+	//		cout<<"Enter new num : ";
+	//		cin>>num;
+	//		list.insertatstart(num);
+	//		cout<<"The list now is : "<<endl;
+	//		list.print();
+	//		cout<<"\n";
+	//	}
+	//	
+	//	cout<<"Enter numbers at end now: ";
+	//	for(int i=0; i<size; i++){
+	//		cout<<"Enter new num : ";
+	//		cin>>num;
+	//		list.insertatend(num);
+	//		cout<<"The list now is : "<<endl;
+	//		list.print();
+	//		cout<<"\n";
+	//	}
+
 }

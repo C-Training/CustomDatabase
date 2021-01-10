2915e78194df2e559a5cffdb1fdff58c6e2bd3c9
yotalha <talha.coder9@gmail.com>
2021-01-10 20:46:02 +0500

    Update fucntion L

--- CustomeDB/CustomeDB.cpp
+++ CustomeDB/CustomeDB.cpp
@@ -1,108 +1,10 @@
-// CustomeDB.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-using namespace std;
-
-
-class Node {
-public:
-    int data;
-    Node* next;
-};
-
-class LinkedList {
-    Node* head = NULL;
-public:
-    void addAtEnd(int val) {
-
-        Node* new_node = new Node();
-        new_node->data = val;
-        new_node->next = NULL;
-        if (head == NULL) {
-            head = new_node;
-        }
-        else {
-            Node* cur = head;
-            while (cur->next != NULL) {
-                cur = cur->next;
-            }
-            cur->next = new_node;
-        }
-
-    }
-    void deleteAtHead() {
-        if (head == NULL) {
-            cout << "There is no linked list to delete" << endl;
-        }
-        else {
-            head = head->next;
-        }
-    }
-
-    void deleteByValue(int x) {
-        if (head == NULL) {
-            cout << "linked list is empty \n";
-        }
-        else if (head->data == x) {
-            deleteAtHead();
-        }
-        else {
-            Node* cur = head;
-            bool found = true;
-            while (cur->data != x) {
-                if (cur == NULL) {
-                    found = false;
-                    break;
-                }
-                cur = cur->next;
-            }
-            if (found) {
-                Node* temp = head;
-                while (temp->next != cur) {
-                    temp = temp->next;
-                }
-                temp->next = cur->next;
-                delete cur;
-            }
-            else {
-                cout << "val doesnt exist in linked list \n";
-            }
-        }
-    }
-
-    void displayList() {
-        Node* cur = head;
-        while (cur != NULL) {
-            cout << cur->data << " ";
-            cur = cur->next;
-        }
-        cout << endl;
-    }
-
-
-};
-
-int main()
-{
-    LinkedList l1;
-    l1.addAtEnd(5);
-    l1.addAtEnd(70);
-    l1.deleteByValue(5);
-    l1.displayList();
-    cout << "Talhe here" << endl;
-
-    return 0;
-}
-/////////////////////////////////////////////////////////////////////
-//Hamza was here L
 #include <iostream>
 #include <string>
 using namespace std;
 
 struct Node {
     int price;
-    string ID;
+    int ID;
     string name;
     Node* next;
 };
@@ -110,7 +12,7 @@ struct Node {
 struct Node* head = NULL;
 
 
-void insert(int prc, string nme, string id) {
+void insert(int prc, string nme, int id) {
     struct Node* newNode = new Node;
     newNode->price = prc;
     newNode->name = nme;
@@ -143,47 +45,45 @@ void deletehead() {
         head = head->next;
     }
 
-    //    void update_data(int old_price,int new_price) {
-    //        int pos = 0;
-    //
-    //        if (head == NULL) {
-    //            cout << "Linked list empty" << endl;
-    //        }
-    //
-    //        current = head;
-    //        while (current->next != NULL) {
-    //            if (current->price == old_price) {
-    //                current->data = new_price;
-    //                cout << "Updated!!!" << endl;
-    //            }
-    //
-    //            current = current->next;
-    //        }
-    //        cout << "not present" << endl;
-    //    }
-    //}
+    
+}
 
-
-    int main() {
-        string p, c;
-        int a, entry = 0;
-        cout << "How many entries you wanna do L" << endl;
-        cin >> entry;
-
-        for (int i = 0;i < entry;i++) {
-            cout << "Enter the product then enter its price and lastly its ID" << endl;
-            cin >> p;
-            cin >> a;
-            cin >> c;
-            insert(a, p, c);
-        }
-        print();
-        deletehead();
-        print();
-        //update_data();
-        //print();
-
-        return 0;
+void update_data(int idval, int price) {
+    struct Node* temp = head;
+    while (temp->ID != idval) {
+        temp = temp->next;
     }
+    temp->price = price;
+}
+
+int main() {
+    //string p;
+    //int c;
+    //int a, entry = 0;
+    //cout << "How many entries you wanna do L" << endl;
+    //cin >> entry;
+
+    //for (int i = 0;i < entry;i++) {
+    //    cout << "Enter the product then enter its price and lastly its ID" << endl;
+    //    cin >> p;
+    //    cin >> a;
+    //    cin >> c;
+    //    insert(a, p, c);
+    //}
+    insert(12,"k",34);
+    insert(34,"ab",32);
+    insert(99,"asv",1);
+    insert(99,"asvs",2);
+    insert(99,"asasda",212);
+    insert(99,"aswe",211);
+    insert(99,"as231",221);
+    insert(99,"asada",241);
+    update_data(211,110);
+    print();
+    //update_data();
+    //print();
+
+    return 0;
+}
 
 ////////////////////////////////////////////////////////

6ba844ae14a62c11653b636d91215978828ca0b1
yotalha <talha.coder9@gmail.com>
2021-01-17 04:40:23 +0500

    MVC Correct implementation.

--- CustomeDB/CustomeDB.cpp
+++ CustomeDB/CustomeDB.cpp
@@ -7,29 +7,43 @@ using namespace std;
 
 class Model {
 public:
-	void enterData(string name, int price) {
-		//this->id = id;
-		this->databaseList.addAtEnd(databaseList.listSize() + 1, name, price);
+	void setPrice(int price) {
+		this->price = price;
 	}
+	void setName(string name){
+		this->name = name;
+	}
+	void setId(int id) { this->id = id; }
 
-	LinkedList getData() {
-		return this->databaseList;
+	int getid() {
+		return this->id;
+	}
+	string getName() {
+		return name;
+	}
+	int getPrice() {
+		return price;
 	}
 
 private:
-	LinkedList databaseList;
+	int id;
+	string name;
+	int price;
 };
 
 
 class View {
 public:
-	void printStudentDetails(Model product) {
-		product.getData().displayList();
+	void printStudentDetails(LinkedList database) {
+		/*product.getData().displayList();*/
+		database.displayList();
+		
 	}
 };
 
 class Controller
 {
+	LinkedList database;
 public:
 	Controller(Model model, View view) {
 		this->model = model;
@@ -37,15 +51,26 @@ public:
 	}
 
 	void setNameAndPrice(string name, int price) {
-		model.enterData(name, price);
+		model.setId(this->database.listSize() + 1);
+		model.setName(name);
+		model.setPrice(price);
+		this->database.addAtEnd(model.getid(), model.getName(), model.getPrice());
 	}
 
-	void getNameAndPrice() {
-		model.getData();
+	/*Node *getNameAndPrice() {
+		return database.getHead();
+	}*/
+
+	void deleteItem() {
+		this->database.deleteAtHead();
+	}
+
+	void updateItem() {
+		this->database.updatePrice(200, 500);
 	}
 
 	void updateView() {
-		view.printStudentDetails(model);
+		view.printStudentDetails(database);
 	}
 
 protected:
@@ -54,24 +79,24 @@ private:
 	View view;
 };
 
-Model retriveStudentFromDatabase() {
-	Model student;
-	student.enterData("Dahi bhallay", 200);
-	student.enterData("pani", 200);
-	student.enterData("Dahi", 300);
-	student.getData().deleteAtHead();
-	return student;
-}
-
 
 int main()
 {
-	Model model = retriveStudentFromDatabase();
-	View view;
+	Model *model = new Model;
+	//model = model->retriveProductFromDatabase();
+	View *view = new View;
 
-	Controller controller(model, view);
 
-	controller.updateView();
+	Controller *controller = new Controller(*model, *view);
+	controller->setNameAndPrice("abad", 500);
+	controller->setNameAndPrice("dahi", 200);
+	controller->setNameAndPrice("bhallay", 200);
+	controller->setNameAndPrice("gg", 700);
+	controller->deleteItem();
+	controller->updateItem();
+	controller->updateView();
+
+	//Test();
 
 	//controller.setStudentName("habshi");
 

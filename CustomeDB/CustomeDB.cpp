e9430363be0503e4041bdebd71adfe60cc66b179
yotalha <talha.coder9@gmail.com>
2021-01-17 01:45:15 +0500

    MVC implementaion with issues in delete at head()

--- CustomeDB/CustomeDB.cpp
+++ CustomeDB/CustomeDB.cpp
@@ -1,31 +1,81 @@
 #include <iostream>
 #include <string>
 #include "LinkedList.h"
-
 #include "Test.h"
 
 using namespace std;
 
+class Model {
+public:
+	void enterData(string name, int price) {
+		//this->id = id;
+		this->databaseList.addAtEnd(databaseList.listSize() + 1, name, price);
+	}
+
+	LinkedList getData() {
+		return this->databaseList;
+	}
+
+private:
+	LinkedList databaseList;
+};
 
 
+class View {
+public:
+	void printStudentDetails(Model product) {
+		product.getData().displayList();
+	}
+};
 
+class Controller
+{
+public:
+	Controller(Model model, View view) {
+		this->model = model;
+		this->view = view;
+	}
+
+	void setNameAndPrice(string name, int price) {
+		model.enterData(name, price);
+	}
+
+	void getNameAndPrice() {
+		model.getData();
+	}
+
+	void updateView() {
+		view.printStudentDetails(model);
+	}
+
+protected:
+private:
+	Model model;
+	View view;
+};
+
+Model retriveStudentFromDatabase() {
+	Model student;
+	student.enterData("Dahi bhallay", 200);
+	student.enterData("pani", 200);
+	student.enterData("Dahi", 300);
+	student.getData().deleteAtHead();
+	return student;
+}
 
 
 int main()
 {
+	Model model = retriveStudentFromDatabase();
+	View view;
 
-    //LinkedList l1("myFile.txt");
-    ///*l1.addAtEnd(1, "talha", 20);
-    //l1.addAtEnd(2, "haseed", 10);
-    //l1.addAtEnd(3, "bats", 25);*/
-    //l1.displayList();
-    //LinkedList l2("newFile.txt");
-    ///*l2.addAtEnd(4, "Abad", 30);*/
-    //l2.displayList();
-    cout << 'g' << endl;
-    Test ();
+	Controller controller(model, view);
 
+	controller.updateView();
 
+	//controller.setStudentName("habshi");
 
-    return 0;
+	//controller.updateView();
 }
+
+

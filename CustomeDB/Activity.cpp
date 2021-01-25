ddf4faa566749ab88cd644b14748b471de630fdb
Talha Asjad <talha.asjad.ta@gmail.com>
2021-01-26 02:29:12 +0500

    Made the database for project class. Made activity class. Data is being inserted at head and tail, deleted from head and tail and being displayed.

--- CustomeDB/Activity.cpp
+++ CustomeDB/Activity.cpp
@@ -11,12 +11,34 @@ void Activity::setData(string name, int id, double salary, string project, strin
 	emp.setAddress(address);
 }
 
+void Activity::setData(string name, int id, string description, int client_id, int money_earned, string deadline, string complete_date) {
+	prj.setname(name);
+	prj.setid(id);
+	prj.setdescription(description);
+	prj.setclient_id(client_id);
+	prj.setmoney_earned(money_earned);
+	prj.setdeadline(deadline);
+	prj.setcomplete_date(complete_date);
+}
+
 void Activity::enterToDatabase()
 {
 	database.addAtEnd(emp.getName(), emp.getId(), emp.getSalary(), emp.getProject(), emp.getJoiningDate(), emp.getPhone(), emp.getAddress());
 }
 
+void Activity::deleteEndProjectDatabase() {
+	database.deleteAtEndPrj();
+}
+
+void Activity::deleteHeadProjectDatabase() {
+	database.deleteAtHeadPrj();
+}
+
+void Activity::enterToDatabasePrj() {
+	database.addAtEnd(prj.getname(), prj.getid(), prj.getdescription(), prj.getclient_id(), prj.getmoney_earned(), prj.getdeadline(), prj.getcomplete_date());
+}
+
 void Activity::displayDatabase()
 {
-	database.displayList();
+	database.displayListPrj();
 }
--- CustomeDB/Node.h
+++ CustomeDB/Node.h
@@ -2,6 +2,7 @@
 #include <string>
 #include "Employee.h"
 #include "client.h"
+#include "Project.h"
 
 using namespace std;
 
@@ -9,14 +10,18 @@ class Node {
 private:
     Employee emp;
     Client client;
+    Project prj;
 public:
-	Node();
     Node();
+    //Node();
     Node(string name, int id, double salary, string project, string joiningDate, string phone, string address);
     Node(string name, int id, string phone, string address, int project_id);
-    Node* next;
+    Node(string name, int id, string description, int client_id, int money_earned, string deadline, string complete_date);
+    //Node* next;
+    //Node* next;
     Node* next;
     Employee getEmployee();
     Client getClient();
+    Project getProject();
 };
 

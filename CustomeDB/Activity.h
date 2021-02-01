e9e3bbce9691040f8e84bb308407748c3074d5f8
hamzafj5 <60666760+hamzafj5@users.noreply.github.com>
2021-02-01 22:23:55 +0500

    Update Activity.h

--- CustomeDB/Activity.h
+++ CustomeDB/Activity.h
@@ -30,7 +30,6 @@ public:
 	void setData(string name, int id, double salary, string project, string joiningDate, string phone, string address);
 	void setData(string name, int id, string phone, string address, int project_id);
 
-	void enterToDatabase();
 	void enterToCliDatabase();
 	
 	void updateClientData(string name, int id, string phone, string address, int project_id);

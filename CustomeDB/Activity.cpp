c445ab019eca0446dc931353f851d779cda7f0aa
hamzafj5 <60666760+hamzafj5@users.noreply.github.com>
2021-02-01 22:21:18 +0500

    Update Activity.cpp

--- CustomeDB/Activity.cpp
+++ CustomeDB/Activity.cpp
@@ -1,16 +1,5 @@
 #include "Activity.h"
 
-void Activity::setData(string name, int id, double salary, string project, string joiningDate, string phone, string address)
-{
-	emp.setName(name);
-	emp.setId(id);
-	emp.setSalary(salary);
-	emp.setProject(project);
-	emp.setJoiningDate(joiningDate);
-	emp.setPhone(phone);
-	emp.setAddress(address);
-	enterToEmployeeDatabase();
-}
 
 
 void Activity::setData(string name, int id, string description, int client_id, string money_earned, string deadline, string complete_date) {
